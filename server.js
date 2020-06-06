93c260a9a4a935d71047f205a66365692c48afc8
Ansh Nagrath <anshnagrath@Anshs-MacBook-Pro.local>
2020-06-06 16:44:39 +0530

    changes in chatroom code

--- server.js
+++ server.js
@@ -1,9 +1,27 @@
-const express = require('express');
+const express = require("express.io");
 const app = express();
-console.log("Server Started")
+const Port = 3000;
+
+console.log(`App is running on port:3000`)
+
+app.http().io();
 app.get('/', function (req, res) {
     res.render('index.ejs');
 });
 app.use(express.static(__dirname + '/public'))
+app.io.route('ready', function (req) {
+    req.io.join(req.data)
+    app.io.room(req.data).broadcast('announce', {
+        message: 'New Client has in the ' + req.data + 'room.'
+    })
 
-app.listen(3000);
+})
+app.io.route('send', function (req) {
+    // console.log("message", req)
+    app.io.room(req.data.room).broadcast('message', {
+        message: req.data.message,
+        author: req.data.author
+    });
+
+})
+app.listen(Port);
